47aa595b1cab4b4c9180a7f948ba804fa436dbc8
Sean <seanmcloughlin9@gmail.com>
2018-01-20 01:26:32 +0000

    Added method to analyst model

--- Models/FightData.Models/DataModels/Analyst.cs
+++ Models/FightData.Models/DataModels/Analyst.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace FightData.Models.DataModels
 {
@@ -8,5 +9,14 @@ namespace FightData.Models.DataModels
         public int Id { get; set; }
         public string Name { get; set; }
         public List<Pick> Picks { get; set; }
+        
+        public string GetNameOfPick(Fight fight)
+        {
+            Pick pick = fight.Picks.FirstOrDefault(p => p.Analyst == this);
+            if (pick != null)
+                return pick.FighterPick.LastName;
+            else
+                return "";
+        }
     }
 }
