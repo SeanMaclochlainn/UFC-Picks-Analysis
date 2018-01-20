db3475800b15957eb70a5d79b2d8b5dce02e9a5a
Sean <seanmcloughlin9@gmail.com>
2018-01-20 21:25:34 +0000

    Folder name change in snapshot

--- DataLayer/FightData.DataLayer/Migrations/20180120212355_foldernamechange.Designer.cs
+++ DataLayer/FightData.DataLayer/Migrations/20180120212355_foldernamechange.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace FightData.DataLayer.Migrations
 {
     [DbContext(typeof(FightPicksContext))]
-    partial class FightPicksContextModelSnapshot : ModelSnapshot
+    [Migration("20180120212355_foldernamechange")]
+    partial class foldernamechange
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DataLayer/FightData.DataLayer/Migrations/20180120212355_foldernamechange.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace FightData.DataLayer.Migrations
+{
+    public partial class foldernamechange : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
